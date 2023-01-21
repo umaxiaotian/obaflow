f1afef6d2f4c92798e7eb78cf8b3efd2610f2470
小畑佑馬 <yuumaobata1008@outlook.com>
2023-01-21 22:50:07 +0900

    MainBarの配色を変更

--- components/MainBar.vue
+++ components/MainBar.vue
@@ -13,18 +13,9 @@
                 <span class="visually-hidden">Icon-only</span>
             </a>
             <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-                <li class="nav-item">
-                    <a href="#" class="nav-link active py-3 " aria-current="page" title=""
-                        data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
-                        <svg width="24"   height="24" xmlns="http://www.w3.org/2000/svg" 
-                            class="bi bi-arrow-down-right-circle-fill" viewBox="0 0 16 16">
-                            <path
-                                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm5.904-2.803a.5.5 0 1 0-.707.707L9.293 10H6.525a.5.5 0 0 0 0 1H10.5a.5.5 0 0 0 .5-.5V6.525a.5.5 0 0 0-1 0v2.768L5.904 5.197z" />
-                        </svg>
-                    </a>
-                </li>
+       
                 <li>
-                    <a href="#" class="nav-link py-3 " title="" data-bs-toggle="tooltip"
+                    <a href="#" class="nav-link py-3 active" title="" data-bs-toggle="tooltip"
                         data-bs-placement="right" data-bs-original-title="Dashboard">
                         <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
                             class="bi bi-arrow-down-right-circle-fill" viewBox="0 0 16 16">
@@ -84,6 +75,13 @@
 </template>
 
 <style scoped>
+.nav-link.active{
+    color: aqua;
+    background-color: #00aed6;
+}
+.nav-link{
+    color: white;
+}
 .target {
   color: red;
   border-bottom: 1px solid currentColor; /* color で指定している色が適応される */
