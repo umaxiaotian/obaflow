fe7875370251ffc4dd71103db9a073485199aacb
小畑佑馬 <yuumaobata1008@outlook.com>
2023-01-22 03:55:44 +0900

    ToolTipsを実装

--- components/MainBar.vue
+++ components/MainBar.vue
@@ -1,20 +1,25 @@
 
 <template>
     <div class="glass_bar">
+
         <div class="d-flex flex-column flex-shrink-0 " style="width: 4.5rem;">
             <a href="/" class="d-block p-3 link-dark text-decoration-none " title="" data-bs-toggle="tooltip"
-                data-bs-placement="right" data-bs-original-title="Icon-only">
+                data-bs-placement="right" data-bs-original-title="スタート">
                 <i class="bi bi-windows" style="font-size: 2.5rem; color: #009eff;"></i>
-                <span class="visually-hidden">Icon-only</span>
+               
             </a>
+
             <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
+
                 <li v-for="menu in menuList">
-                    <div class='nav-link py-3 mb-1 ' :class="{ 'active': menu.isActive === true }"
-                        @click="menuClickFunc(menu.funcName)" style="border: 1px solid rgba(255, 255, 255, 0.5);"
-                        title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
+                    <a class='nav-link py-3  ' :class="{ 'active': menu.isActive === true }"
+                        @click="menuClickFunc(menu.funcName)" data-bs-toggle="tooltip" data-bs-placement="right"
+                        :title="menu.toolTipText">
                         <i :class="menu.iconClass" style="font-size: 1.5rem; color: #ffff;"></i>
-                    </div>
+                    </a>
+
                 </li>
+
             </ul>
             <div class="dropdown border-top">
                 <a href="#" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none "
@@ -44,6 +49,7 @@
     background-color: #174051;
 }
 
+
 .target {
     color: red;
     border-bottom: 1px solid currentColor;
@@ -68,7 +74,10 @@
 </style>
 
 <script>
+import "bootstrap/dist/js/bootstrap.min.js";
+import { Tooltip } from 'bootstrap/dist/js/bootstrap.esm.min.js'
 export default {
+    name: "MainBar",
     model: {
         prop: "editor",
         event: "change",
@@ -89,15 +98,21 @@ export default {
         },
     },
 
-
     data() {
         return {
             menuList: {
-                'test': { iconClass: 'bi bi-apple', isActive: false, funcName: 'saveEditorInfo' },
-                'open': { iconClass: 'bi bi-apple', isActive: false, funcName: 'openEditorInfo' }
+                'test': { iconClass: 'bi bi-cloud-download', isActive: false, funcName: 'saveEditorInfo' ,toolTipText:"エディター情報からデータをダウンロードします。" },
+                'open': { iconClass: 'bi bi-cloud-arrow-up', isActive: false, funcName: 'openEditorInfo' ,toolTipText:"エディター情報からデータをアップロードします。"}
             }
         }
     },
+
+    mounted() {
+        //inti tooltip
+        Array.from(document.querySelectorAll('a[data-bs-toggle="tooltip"]'))
+            .forEach(tooltipNode => new Tooltip(tooltipNode))
+    },
+
     methods: {
         menuClickFunc(event) {
             switch (event) {
@@ -108,7 +123,7 @@ export default {
                     this.menuList.test.isActive = true;
                     break
                 case 'openEditorInfo':
-                    state={"nodes":[{"type":"DataCopyNode","id":"node_16743213464480","name":"DataCopyNode","options":[["ValueText",""],["ローカルファイルパス",null],["アップロード先パス",null],["CHMOD",null],["ファイル所有者名",null]],"state":{},"interfaces":[["Play",{"id":"ni_16743213464481","value":""}]],"position":{"x":560,"y":312},"width":200,"twoColumn":false,"customClasses":""},{"type":"PlayNode","id":"node_16743213647492","name":"Play","options":[["Play名",null],["ユーザー切替有効",null],["実行ユーザー名",null],["ホスト群",null]],"state":{},"interfaces":[["Task",{"id":"ni_16743213647493","value":null}]],"position":{"x":247,"y":323},"width":200,"twoColumn":false,"customClasses":""}],"connections":[],"panning":{"x":0,"y":0},"scaling":1};
+                    state = { "nodes": [{ "type": "DataCopyNode", "id": "node_16743213464480", "name": "DataCopyNode", "options": [["ValueText", ""], ["ローカルファイルパス", null], ["アップロード先パス", null], ["CHMOD", null], ["ファイル所有者名", null]], "state": {}, "interfaces": [["Play", { "id": "ni_16743213464481", "value": "" }]], "position": { "x": 560, "y": 312 }, "width": 200, "twoColumn": false, "customClasses": "" }, { "type": "PlayNode", "id": "node_16743213647492", "name": "Play", "options": [["Play名", null], ["ユーザー切替有効", null], ["実行ユーザー名", null], ["ホスト群", null]], "state": {}, "interfaces": [["Task", { "id": "ni_16743213647493", "value": null }]], "position": { "x": 247, "y": 323 }, "width": 200, "twoColumn": false, "customClasses": "" }], "connections": [], "panning": { "x": 0, "y": 0 }, "scaling": 1 };
                     this.editor.load(state);
                     break
 
